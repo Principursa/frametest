7a0752ebd0a3dd2c9dd855da4513f73f31c0e4c1
Pattisier <Principursa@protonmail.com>
2024-02-17 20:54:16 -0500

    djflsjfl

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css'
+import {FrameMetadata, FrameRequest, getFrameAccountAddress, getFrameMessage } from '@coinbase/onchainkit';
 
 function App() {
 
   return (
-    <p>testing123</p>
+    <FrameMetadata
+      buttons={[
+        {
+          label: 'Tell me the story',
+        },
+        {
+          action: 'link',
+          label: 'Link to Google',
+          target: 'https://www.google.com'
+        },
+        {
+          action: 'post_redirect',
+          label: 'Redirect to cute pictures',
+        },
+      ]}
+      image={{
+       src: 'https://zizzamia.xyz/park-3.png',
+       aspectRatio: '1:1'
+      }}
+      input={{
+        text: 'Tell me a boat story',
+      }}
+      postUrl="https://zizzamia.xyz/api/frame"
+    />
 
+   
    
   )
 }
