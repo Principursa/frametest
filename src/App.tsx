5c500715982607b93cf1e0a72ff5f94ed6e7bef4
Pattisier <Principursa@protonmail.com>
2024-02-17 20:03:00 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {FrameMetadata} from "@coinbase/onchainkit"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p>testing123</p>
+
+   
+  )
+}
+
+export default App
